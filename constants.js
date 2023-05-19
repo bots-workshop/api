48b856fa27cd70b9f65ed124b59548c1dc51a92e
danlaptev <danlaptev@yandex-team.ru>
2023-05-19 23:49:04 +0300

    изменить baseUrl

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
 export const PORT = 8001;
 
-export const BASE_URL = process.env.NODE_ENV === 'production' ? '' : `http://localhost:${PORT}/`;
+export const BASE_URL = process.env.NODE_ENV === 'production' ? `http://51.250.41.6:${8001}` : `http://localhost:${PORT}/`;
